5b81046c988db6676c417a9484f2af965588f748
erikalam <erikaklam@gmail.com>
2022-05-10 15:02:45 -0400

    Create recipe select listview

--- app/src/main/java/com/example/mealgo3/domain/MainActivity.java
+++ app/src/main/java/com/example/mealgo3/domain/MainActivity.java
@@ -6,13 +6,15 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.mealgo3.R;
 import com.example.mealgo3.databinding.ActivityMainBinding;
-import com.example.mealgo3.databinding.ActivitySignInBinding;
-import com.example.mealgo3.login.SignUpActivity;
 import com.example.mealgo3.nav.MenuFragment;
 import com.example.mealgo3.search.SearchFragment;
+import com.example.mealgo3.selectrecipes.RecipeSelect;
 
 public class MainActivity extends AppCompatActivity implements SearchFragment.OnFragmentInteractionListener {
 
@@ -25,13 +27,20 @@ public class MainActivity extends AppCompatActivity implements SearchFragment.On
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
         setListeners();
         loadSearch();
     }
 
     private void setListeners() {
         binding.buttonMenu.setOnClickListener(v -> openMenu());
+
+        Button button = (Button) findViewById(R.id.recipeRecs);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), RecipeSelect.class));
+            }
+        });
     }
 
     private void openMenu() {
@@ -57,4 +66,6 @@ public class MainActivity extends AppCompatActivity implements SearchFragment.On
     public void onFragmentInteraction(String sendBackText) {
         System.out.println(sendBackText);
     }
+
+
 }
