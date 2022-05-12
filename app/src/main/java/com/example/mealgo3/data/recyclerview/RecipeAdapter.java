3e9f05d06bf54aafa848f5d47ffb5c95e3e25ad2
sim15 <sbeyzerov@gmail.com>
2022-05-12 17:35:11 -0400

    Add recipe filtering

--- app/src/main/java/com/example/mealgo3/data/recyclerview/RecipeAdapter.java
+++ app/src/main/java/com/example/mealgo3/data/recyclerview/RecipeAdapter.java
@@ -1,5 +1,6 @@
 package com.example.mealgo3.data.recyclerview;
 
+import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -7,25 +8,37 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.mealgo3.R;
 import com.example.mealgo3.data.Recipe;
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipGroup;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Stream;
 
 
 public class RecipeAdapter extends FirestoreRecyclerAdapter<Recipe, RecipeAdapter.RecipeHolder> {
     private OnItemClickListener listener;
+    private ArrayList<Map<String, ArrayList<String>>> categories;
+    private ArrayList<String> toExclude;
 
-
-    public RecipeAdapter(@NonNull FirestoreRecyclerOptions<Recipe> options) {
+// , Set<String> inclusionFilter, Set<String> exclusionFilter
+    public RecipeAdapter(@NonNull FirestoreRecyclerOptions<Recipe> options, ArrayList<Map<String, ArrayList<String>>> categories, ArrayList<String> exclusionFilter) {
         super(options);
+        this.categories = categories;
+        toExclude = exclusionFilter;
     }
 
     @Override
@@ -38,6 +51,35 @@ public class RecipeAdapter extends FirestoreRecyclerAdapter<Recipe, RecipeAdapte
 
         holder.RecipeName.setText(toSet);
         holder.RecipeDescription.setText(model.getRecipeCategory());
+
+        ArrayList<String> matches = returnMatchingCategories(model);
+
+        if (matches.size() == 0) {
+            holder.view.setVisibility(View.GONE);
+            holder.itemView.setLayoutParams(new RecyclerView.LayoutParams(0, 0));
+            return;
+        } else {
+            System.out.println(matches);
+            // This conditional branch is required due to the recycling of view holders
+            // otherwise, other views will continue to be hidden when recycled.
+            holder.itemView.setVisibility(View.VISIBLE);
+            holder.itemView.setLayoutParams(new RecyclerView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        }
+
+        holder.MatchedCategories.removeAllViews();
+
+//        for (int i = 0; i < holder.MatchedCategories.getChildCount(); i++) {
+//            holder.MatchedCategories.removeView(holder.MatchedCategories.getChildAt(i));
+//        }
+
+        for (String matchedCat : matches) {
+//            Chip categoryChip = (Chip) getLayoutInflater().inflate(R.layout.category_chip, holder.MatchedCategories, false);
+            Chip categoryChip = new Chip(holder.MatchedCategories.getContext());
+            categoryChip.setText(matchedCat);
+            categoryChip.setCheckable(false);
+            categoryChip.setClickable(false);
+            holder.MatchedCategories.addView(categoryChip);
+        }
 
         if (model.getImages().size() > 0) {
             Picasso
@@ -64,6 +106,7 @@ public class RecipeAdapter extends FirestoreRecyclerAdapter<Recipe, RecipeAdapte
         TextView RecipeName;
         TextView RecipeDescription;
         ImageView RecipePreview;
+        ChipGroup MatchedCategories;
 
 
         public RecipeHolder(@NonNull View itemView) {
@@ -71,8 +114,10 @@ public class RecipeAdapter extends FirestoreRecyclerAdapter<Recipe, RecipeAdapte
             RecipeName = itemView.findViewById(R.id.recipeName);
             RecipeDescription = itemView.findViewById(R.id.recipeDescription);
             RecipePreview = itemView.findViewById(R.id.recipePreview);
+            MatchedCategories = itemView.findViewById(R.id.matchedCategories);
 
-//            view = itemView;
+
+            view = itemView;
 
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -107,5 +152,53 @@ public class RecipeAdapter extends FirestoreRecyclerAdapter<Recipe, RecipeAdapte
     public int getItemViewType(int position) {
         return position;
     }
+
+    private ArrayList<String> returnMatchingCategories(Recipe r) {
+
+        ArrayList<String> matches = new ArrayList<String>();
+
+        if ((toExclude.size() > 0) && !Collections.disjoint(r.getIngredients(), toExclude)) {
+            System.out.println(r.getName() + "- exclusion!");
+            System.out.println(r.getIngredients());
+            System.out.println(toExclude);
+            return matches;
+        }
+
+
+
+        for (Map<String, ArrayList<String>> category : categories) {
+            System.out.println(Collections.disjoint(r.getIngredients(), category.get("include")));
+
+            if ((category.get("include").size() == 0) ||
+                    !Collections.disjoint(r.getIngredients(), category.get("include"))) {
+
+                System.out.println(r.getName() + "- match include!");
+                System.out.println(r.getIngredients());
+                System.out.println(category.get("include"));
+                if ((category.get("exclude").size() < 1) ||
+                        Collections.disjoint(r.getIngredients(), category.get("exclude"))) {
+                    matches.add(category.get("categoryName").get(0));
+                }
+            }
+        }
+
+        return matches;
+
+    }
+
+//    public void filter(String text) {
+//        items.clear();
+//        if(text.isEmpty()){
+//            items.addAll(itemsCopy);
+//        } else{
+//            text = text.toLowerCase();
+//            for(PhoneBookItem item: itemsCopy){
+//                if(item.name.toLowerCase().contains(text) || item.phone.toLowerCase().contains(text)){
+//                    items.add(item);
+//                }
+//            }
+//        }
+//        notifyDataSetChanged();
+//    }
 
 }
